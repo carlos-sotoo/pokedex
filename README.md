55f9f867517e6df2e93fb5a75c9e7e6975d6e488
Carlos Soto <carlos950216@gmail.com>
2022-07-06 20:07:02 -0500

    responsive menu

--- README.md
+++ README.md
@@ -16,8 +16,9 @@ El proyecto fue realizado con las siguientes librerias de react js:
 * [react-icons](https://react-icons.github.io/react-icons/) - Es una libreria que ayuda a incluir iconos a un proyecto react usandolos y personalizandolos de una forma sencilla.
 
 ## Lenguajes y herramientas:
+<p align="left">
 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> 
 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> 
 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> 
 <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg" alt="react" width="40" height="40"/> 
-<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/>
+<img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,60 +1,60 @@
+import { useRef } from 'react'
 import { Link, useSearchParams, useNavigate, useParams } from 'react-router-dom'
 import pokemonLogo from '../images/pokemon-logo.png'
 import '../styles/components/header.scss'
+
+const types = ["default","normal","fighting","flying","poison","ground","rock","bug","ghost","steel","fire","water","grass","electric","psychic","ice","dragon","dark","fairy","unknown","shadow"]
 
 export const Header = () => {
     const navigate = useNavigate()
     const [searchParams, setSearchParams] = useSearchParams()
     const { name } = useParams()
-    const types = [ "default",
-                    "normal",
-                    "fighting",
-                    "flying",
-                    "poison",
-                    "ground",
-                    "rock",
-                    "bug",
-                    "ghost",
-                    "steel",
-                    "fire",
-                    "water",
-                    "grass",
-                    "electric",
-                    "psychic",
-                    "ice",
-                    "dragon",
-                    "dark",
-                    "fairy",
-                    "unknown",
-                    "shadow"]
+    const refBtn = useRef(null)
+    const refNav = useRef(null)
 
     const search = searchParams.get('search') ?? ""
 
-    const handleFilter = e =>{
-        setSearchParams({search:e.target.value.toLowerCase()})
+    const handleFilter = e => {
+        setSearchParams({ search: e.target.value.toLowerCase() })
     }
 
-    const handleType = e =>{
-        if(e.target.value === 'default')
+    const handleType = e => {
+        if (e.target.value === 'default')
             navigate('/pokemons')
         else
             navigate(`/type/${e.target.value}`)
     }
 
+    const handleMenuBtn=(e)=>{
+        let btn = refBtn.current
+        let nav = refNav.current
+        
+        if(!btn.className.includes("active")){
+            btn.classList.add("active")
+            nav.classList.add("active")
+        }else{
+            btn.classList.remove("active")
+            nav.classList.remove("active")
+        }
+            
+    }
+
     return (
         <header className='header'>
-            <Link to='/'><img src={pokemonLogo} alt='pokemon logo'/></Link>
-             
-            <input
-                className='searchInput'
-                type='search' 
-                placeholder='Enter name or id Pokémon' 
-                onChange={handleFilter}
-                value={search}
-            />
-            <select className='typeInput' onChange={handleType} value={name}>
-                {types.map(type=><option key={type} value={type}>{type}</option>)}
-            </select>
+            <Link to='/'><img src={pokemonLogo} alt='pokemon logo' /></Link>
+            <button ref={refBtn} className='btn' onClick={handleMenuBtn}><span> </span></button>
+            <nav ref={refNav}>
+                <input
+                    className='searchInput'
+                    type='search'
+                    placeholder='Enter name or id Pokémon'
+                    onChange={handleFilter}
+                    value={search}
+                />
+                <select className='typeInput' onChange={handleType} value={name}>
+                    {types.map(type => <option key={type} value={type}>{type}</option>)}
+                </select>
+            </nav>
         </header>
     )
 }
